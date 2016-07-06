75a6ab186d60b4439e887be285811692c96cd0d6
Daniel Siepmann <d.siepmann@web-vision.de>
2016-07-06 17:59:01 +0200

    FEATURE: Provide first working version to parse github

--- typo3Git.py
+++ typo3Git.py
@@ -5,8 +5,9 @@ import scrapy
 
 class TYPO3GitSpider(scrapy.Spider):
     name = 'typo3gitsionspider'
+    branch_information = 'https://github.com/TYPO3/TYPO3.CMS/branch_commits/'
     start_urls = [
-            'https://github.com/TYPO3/TYPO3.CMS/commits/master',
+        'https://github.com/TYPO3/TYPO3.CMS/commits/master',
     ]
 
     def parse(self, response):
@@ -14,25 +15,44 @@ class TYPO3GitSpider(scrapy.Spider):
             full_url = response.urljoin(href.extract())
             yield scrapy.Request(full_url, callback=self.parse_commit_list)
 
+    # TODO: Fetch all following pages
     def parse_commit_list(self, response):
+        for href in response.css('.pagination a::attr(href)'):
+            full_url = response.urljoin(href.extract())
+            yield scrapy.Request(full_url, callback=self.parse_commit_list)
+
         for href in response.css('.commit-group a.sha::attr(href)'):
             full_url = response.urljoin(href.extract())
             yield scrapy.Request(full_url, callback=self.parse_commit_details)
 
     def parse_commit_details(self, response):
-        # Generate Ajax call for scraping?
-        # Use commit hash for url and identification, just store what we need here and store association seperate?
-        # branches = response.css('.branches-tag-list a::text').extract()
-        # if (len(branches) == 0):
-        #     print " !> No branches found for", response
-        # else:
-        #     print "!!!! Branches:", branches
-        # yield {
-        #     'version': response.css('.full-commit .branches-tag-list a:::text').extract()[:3],
-        #     'changes': {
-        #         'breaking': len(response.css('#breaking-changes a.reference.internal::text').extract()),
-        #         'deprecation': len(response.css('#deprecation a.reference.internal::text').extract()),
-        #         'feature': len(response.css('#features a.reference.internal::text').extract()),
-        #         'important': len(response.css('#important a.reference.internal::text').extract()),
-        #     }
-        # }
+        title = response.css('.commit-title ::text').extract_first().strip()
+
+        commit = {
+            'type': self.get_commit_type(title),
+            'title': title[title.rfind(']')+2:],
+            'hash': response.css('.sha-block .sha.user-select-contain::text').extract_first(),
+        }
+
+        return [scrapy.Request(self.branch_information + commit['hash'], callback=self.parse_commit_branch, meta={'commit': commit})]
+
+    def parse_commit_branch(self, response):
+        commit = response.meta['commit']
+
+        # TODO: Fix issue:
+        #    2016-07-06 17:53:23 [scrapy] ERROR: Spider error processing <GET https://github.com/TYPO3/TYPO3.CMS/branch_commits/2bc918df67213c8539a768647f279c8861aa5fa6> (
+        #    referer: https://github.com/TYPO3/TYPO3.CMS/commit/2bc918df67213c8539a768647f279c8861aa5fa6)
+        #    Traceback (most recent call last):
+        #    File "/Users/siepmann/Library/Python/2.7/lib/python/site-packages/twisted/internet/defer.py", line 588, in _runCallbacks
+        #        current.result = callback(current.result, *args, **kw)
+        #    File "/Users/siepmann/Projects/own/t3-stats/typo3Git.py", line 38, in parse_commit_branch
+        #        commit['version'] = response.css('.branches-tag-list a::text').extract()[-1][:3]
+        #    IndexError: list index out of range
+        commit['version'] = response.css('.branches-tag-list a::text').extract()[-1][:3]
+
+        return [commit]
+
+    def get_commit_type(self, title):
+        # TODO: return usable information like "!!! FEATURE" -> "Breaking Change"?
+        # Check which exist
+        return title[1:title.rfind(']')].replace('][', ' '),
